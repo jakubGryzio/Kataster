8de106004fc19e959dbcdbd9f0898e0cb9bac375
JakubGryzio <jakubgryzio@gmail.com>
2021-10-31 00:10:49 +0200

    Developing front-end
    Creating header and main front-end side of application. Develop index.html and style.css.

---
+++ SyntaxValidator/script.js
@@ -0,0 +1,8 @@
+"use strict";
+
+// const btnCheck = document.querySelector("#submit");
+// const info = document.querySelector(".info");
+
+// btnCheck.addEventListener("click", () => {
+//   info.classList.remove("hidden");
+// });
